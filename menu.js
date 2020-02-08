ed4a3f4d5a026da10bacfff3bd9999b77deaa9ec
mcdDejan <dwkua@live.com>
2020-02-08 19:37:21 +0100

    nesto

--- menu.js
+++ menu.js
@@ -1,4 +1,7 @@
 let mainContainer = $(".main-container");
+
+
+
 createMenuContainer = () => {
     let menuContainer = $("<div>").attr("class", "menu-container");
     let leftMenu = $("<div>").attr("class", "left-menu");
@@ -7,4 +10,10 @@ createMenuContainer = () => {
     $(menuContainer).append(leftMenu);
     $(menuContainer).append(rightMenu);
     $(mainContainer).append(menuContainer);
-}
+};
+
+createAboutContainer = () => {
+    let about = $("<div>").attr("class", "about");
+    $(mainContainer).append(about);
+};
+
