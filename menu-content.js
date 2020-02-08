0951bd955127a0d0fddf5e5c03a521281056daf2
JovanStef <jov.box@outlook.com>
2020-02-08 19:30:50 +0100

    menu-content

---
+++ menu-content.js
@@ -0,0 +1,104 @@
+getMenuContent=()=>{
+    const menu = {
+        dayDish:{
+            header:'Наш предлог',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+
+        },
+        bbq:{
+            header:'Скара',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+        },
+        salad:{
+            header:'Салати',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+        },
+        sesonSalad:{
+            header:'Сезонски салати',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+        },
+        beer:{
+            header:'Пиво',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+        },
+        spirits:{
+            header:'Жестоко',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+        },
+        vine:{
+            header:'Вина',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+        },
+        softDrinks:{
+            header:'Сокови',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+        },
+        coffe:{
+            header:'Кафе',
+            img:'our-menu.jpg',
+            list:[
+                {foodName:'Џигер',foodPrice:'120'},
+                {foodName:'Мучкалица',foodPrice:'160/180'},
+                {foodName:'Лута плескавица',foodPrice:'120'},
+                {foodName:'Микс Убац',foodPrice:'120'}
+            ]
+        }
+
+
+
+
+
+
+
+    }
+    return menu
+
+}
