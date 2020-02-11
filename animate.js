c4b0119779fe68e44f093f260eb7e74db4a1ff23
JovanStef <jov.box@outlook.com>
2020-02-11 08:51:31 +0100

    css nesto

--- animate.js
+++ animate.js
@@ -11,16 +11,18 @@ animateBeerMug=()=>{
             $('.leva-krigla').removeClass('open-close-left');
             $('.desna-krigla').removeClass('open-close-right');
     
-        },7000);
-    }
-    $('.leva-krigla').addClass('open-left');
-    $('.desna-krigla').addClass('open-right');
+        },2000);
+    }else{
 
+        $('.leva-krigla').addClass('open-left');
+        $('.desna-krigla').addClass('open-right');
+        
     setTimeout(()=>{
         $('.leva-krigla').css({'left':'-50vw'});
         $('.desna-krigla').css({'right':'-50vw'});
-
-    },7000);
+        
+    },3000);
+}
     
 
 };
