656d8d08d161c16f3832286ee00ff0841f6e9015
unknown <dwkua@live.com>
2020-02-12 22:38:29 +0100

    mobile

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ $( document ).ready(function() {
     let menuContent = getMenuContent();
 
     $(".hamburger").click((event) => {
+        
+        animateBeerMug();
+        
         if ($(".menu").css("display") == "none") {
             $(".menu").css({"display": "flex"});
         } else {
@@ -10,8 +13,23 @@ $( document ).ready(function() {
             var classState = $(event.target).css("color")
             console.log(classState);
         };
+
+        if ($(".menu-container").css("display") == "flex") {
+            $(".menu-container").css({"display": "none"});
+        }else {
+            $(".menu-container").css({"display": "flex"});
+        };
         
     });
+
+
+    $(".menu li").click((event) => {
+        if (screen.width < 767) {
+            $(".menu").css({"display": "none"});
+        };
+    });
+
+
     
     $(".menu li").click((event) => {
         animateBeerMug();
@@ -19,7 +37,7 @@ $( document ).ready(function() {
         
         var elementId = $(event.target).attr("id")
         $(".main-container").html(" ");
-
+    
         switch(elementId) {
             case 'home':           
                 $(".main-page-info").css({"display": "block"});
@@ -29,23 +47,31 @@ $( document ).ready(function() {
               break;
               case 'about':
                 createAboutContainer();
+                $(".main-page-info").css({"display": "block"});
+                
                 break;
                 case 'gallery':
                     // code block
                     break;
             default:
               // code block
-          } 
+          };
         
+
         $("#food-menu li").click((event) => {
+            if (screen.width < 767) {
+                $(".left-menu").css({"display": "none"});
+            }
             $(".right-menu").html(" ");
             let elementId = $(event.target).attr("id");
             let content = createRightMenuContent(menuContent[elementId]);
             $(".right-menu").append(content);
-    
-    
-        })
+        });
+
     });
+    
+
+    
  
 
     // $("#food-menu li").click(function() {
